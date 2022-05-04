cbfcf030e2ee79b8f692b9ed399967271c5b78b0
antonin06390 <104512626+antonin06390@users.noreply.github.com>
2022-05-04 11:11:13 +0200

    Add files via upload

---
+++ myApp/config.py
@@ -0,0 +1,18 @@
+ENV="developpment"
+DEBUG=True
+SEND_FILE_MAX_AGE_DEFAULT = 0
+SECRET_KEY = "maCleSuperSecurisee"
+
+WEB_SERVER = {
+    "host" : "localhost",
+    "port" : 8080,
+}
+
+DB_SERVER = {
+    "user" : "root",
+    "password" : "",
+    "host" : "localhost",
+    "port" : 3306,
+    "database" : "BDD",
+    "raise_on_warnings" : True
+}
